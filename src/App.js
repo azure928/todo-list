af408887288f6eab71aa86b2fa5908faa0cc7ffa
Ji Yook <azure6.dev@gmail.com>
2023-10-20 15:37:57 +0900

    Todo 객체로 관리하는것, checkbox와 연동 - state는 특정부분의 변경이 아니라, 새로운 값으로 업데이트 되어야함

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 import { useState } from "react";
-​
+
 // camelcase -> 띄어씌기가 필요한 곳에 대문자로 표시한다
 // ex) background-color -> backgroundColor
 //     font-size -> fontSize
@@ -9,19 +9,21 @@ function App() {
   // react way
   const [inputValue, setInputValue] = useState("");
   /**
-   * TODO
+   * TODO object, 객체
    * {
-   *  id: 중복되지 않는 id값,
+   *  id: 중복되지 않는 id값, uuid 항상 고유한 키값이 나오는 암호화 로직을 갖고 있는 라이브러리
    *  content: 할일에 대해 적은 내용 strign ('할일 1')
    *  createdAt: 생성된 시간 number (0~9128439184921490)
    *  isDone: 완료 여부 boolean (true, false)
    * }
    */
+
   const [todos, setTodos] = useState([]);
+  console.log({ todos });
   // state 변화하는 값, 임시 값
   // React -> state가 변할때마다 화면을 다시 그린다.
-  // ["할일 1", "할일 2"];
-​
+  // ["할일 1", "할일 2"] -> ["할일 1", "할일 2", "할일 3"]];
+
   return (
     // JSX (JS -> HTML)
     <div className="App">
@@ -39,36 +41,50 @@ function App() {
         <button
           onClick={() => {
             // spread 연산자
-            /**
-             * TODO
-             * {
-             *  id: 중복되지 않는 id값,
-             *  content: 할일에 대해 적은 내용
-             *  createdAt: 생성된 시간
-             *  isDone: 완료 여부
-             * }
-             * */
-​
-            // uuid 항상 공유한 키값이 나오는 암호화 로직을 갖고 있는 라이브러리
-​
-            setTodos([...todos, {}]);
+
+            const newTodo = {
+              id: uuid(),
+              content: inputValue,
+              createdAt: Date.now(),
+              isDone: false,
+            };
+            setTodos([...todos, newTodo]);
             setInputValue("");
           }}
         >
           ADD
         </button>
       </div>
+
       {/* DRY Don't Repeat Yourself */}
-      {/* [할일 1, 할일 2]  */}
+      {/* [할일 1, 할일 2, 할일 3]  */}
       {todos.map((todo, index) => (
-        <div key={index}>
-          <input type="checkbox" />
-          <span>{todo}</span>
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.isDone}
+            onChange={(e) => {
+              /**
+               * todos :
+               * { content : "할일 1"
+               * createdAt : 1697782607507
+               * id : "38b5c80a-008a-47f4-81f1-bb50fa206c29"
+               * isDone : false },
+               * 두번째 todo,
+               * 서밴째 todo
+               */
+              const nextTodos = todos.map((todo, idx) =>
+                idx === index ? { ...todo, isDone: e.target.checked } : todo
+              );
+              setTodos(nextTodos);
+            }}
+          />
+          <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.content}</span>
           <button>DEL</button>
         </div>
       ))}
     </div>
   );
 }
-​
+
 export default App;
